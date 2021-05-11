e78e7eda9413b08dc324a2dee8aaf65aa8b55117
Carl Montanari <carl.r.montanari@gmail.com>
2021-05-10 18:50:00 -0700

    formatting

--- tests/unit/transport/test_ssh2.py
+++ tests/unit/transport/test_ssh2.py
@@ -9,5 +9,6 @@ from scrapli_netconf.driver import NetconfDriver
 def test_init():
     # importing in here for now to not break 3.10 tests
     from scrapli_netconf.transport.plugins.ssh2.transport import NetconfSsh2Transport
+
     conn = NetconfDriver(host="localhost", transport="ssh2")
     assert isinstance(conn.transport, NetconfSsh2Transport)

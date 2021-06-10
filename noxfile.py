73aa07ac8ae584323635ecaab8451dcbdde4120c
Carl Montanari <carl.r.montanari@gmail.com>
2021-06-10 07:42:13 -0700

    bump mypy, remove unused pin, skip ssh2 on darwin >3.9

--- noxfile.py
+++ noxfile.py
@@ -61,7 +61,6 @@ REQUIREMENTS: Dict[str, str] = parse_requirements(dev=False)
 DEV_REQUIREMENTS: Dict[str, str] = parse_requirements(dev=True)
 PLATFORM: str = sys.platform
 SKIP_LIST: List[str] = [
-    "unit_tests-darwin-3.10",
     "integration_tests-darwin-3.10",
 ]
 

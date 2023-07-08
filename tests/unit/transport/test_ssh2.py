180952b6ef9fe7991d91057f52bd87819b7019a6
Carl Montanari <carl.r.montanari@gmail.com>
2023-07-08 09:15:06 -0700

    chore: run ssh2 init test on 3.11

--- tests/unit/transport/test_ssh2.py
+++ tests/unit/transport/test_ssh2.py
@@ -5,7 +5,7 @@ import pytest
 from scrapli_netconf.driver import NetconfDriver
 
 
-@pytest.mark.skipif(sys.version_info > (3, 10), reason="skipping ssh2 on 3.11")
+@pytest.mark.skipif(sys.version_info > (3, 11), reason="skipping ssh2 on 3.12")
 def test_init():
     # importing in here for now to not break 3.11 tests
     from scrapli_netconf.transport.plugins.ssh2.transport import NetconfSsh2Transport

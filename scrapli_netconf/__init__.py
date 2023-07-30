23ffe23b7fb44839f0779c4a230e13d100846634
Carl Montanari <carl.r.montanari@gmail.com>
2023-07-30 10:00:05 -0700

    chore: housekeeping, verison bump

--- scrapli_netconf/__init__.py
+++ scrapli_netconf/__init__.py
@@ -1,7 +1,7 @@
 """scrapli_netconf"""
 from scrapli_netconf.driver import AsyncNetconfDriver, NetconfDriver
 
-__version__ = "2023.01.30"
+__version__ = "2023.07.30"
 
 __all__ = (
     "NetconfDriver",

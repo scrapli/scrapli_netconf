3888a7fe004921ce14714e69b50b419cee45f689
Carl Montanari <carl.r.montanari@gmail.com>
2025-01-30 16:36:47 -0800

    chore: bump versions for ci things, prep for release

--- noxfile.py
+++ noxfile.py
@@ -81,7 +81,7 @@ def _get_install_test_args() -> List[str]:
     return args
 
 
-@nox.session(python=["3.8", "3.9", "3.10", "3.11", "3.12", "3.13"])
+@nox.session(python=["3.9", "3.10", "3.11", "3.12", "3.13"])
 def unit_tests(session):
     """
     Nox run unit tests
@@ -115,7 +115,7 @@ def unit_tests(session):
     )
 
 
-@nox.session(python=["3.11"])
+@nox.session(python=["3.13"])
 def isort(session):
     """
     Nox run isort
@@ -135,7 +135,7 @@ def isort(session):
     session.run("python", "-m", "isort", "-c", ".")
 
 
-@nox.session(python=["3.11"])
+@nox.session(python=["3.13"])
 def black(session):
     """
     Nox run black
@@ -155,7 +155,7 @@ def black(session):
     session.run("python", "-m", "black", "--check", ".")
 
 
-@nox.session(python=["3.11"])
+@nox.session(python=["3.13"])
 def pylint(session):
     """
     Nox run pylint
@@ -174,7 +174,7 @@ def pylint(session):
     session.run("python", "-m", "pylint", "scrapli_netconf/")
 
 
-@nox.session(python=["3.11"])
+@nox.session(python=["3.13"])
 def pydocstyle(session):
     """
     Nox run pydocstyle
@@ -194,7 +194,7 @@ def pydocstyle(session):
     session.run("python", "-m", "pydocstyle", ".")
 
 
-@nox.session(python=["3.11"])
+@nox.session(python=["3.13"])
 def mypy(session):
     """
     Nox run mypy
@@ -215,7 +215,7 @@ def mypy(session):
     session.run("python", "-m", "mypy", "--strict", "scrapli_netconf/")
 
 
-@nox.session(python=["3.11"])
+@nox.session(python=["3.13"])
 def darglint(session):
     """
     Nox run darglint
